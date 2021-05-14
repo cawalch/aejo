8d0dc341e30f834472876a2c8c77e39df3793e27
Caleb Walch <cawalch@pm.me>
2021-05-14 10:00:14 -0500

    release-0.1.9

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,49 +5,48 @@ import express, {
   RequestHandler,
   NextFunction,
   Router,
-} from "express";
-import * as OpenAPI3 from "../types/open-api-3";
-// import { JsonObject } from "swagger-ui-express";
-import Ajv, { SchemaObject, ValidateFunction } from "ajv";
-import addFormats from "ajv-formats";
-import { ValidationError } from "./errors";
+} from 'express'
+import * as OpenAPI3 from '../types/open-api-3'
+import Ajv, { SchemaObject, ValidateFunction } from 'ajv'
+import addFormats from 'ajv-formats'
+import { ValidationError } from './errors'
 
-export const ajv = new Ajv();
-addFormats(ajv);
+export const ajv = new Ajv()
+addFormats(ajv)
 
-export type AppRoute = { paths: OpenAPI3.PathItem[]; router: Router };
+export type AppRoute = { paths: OpenAPI3.PathItem[]; router: Router }
 
-export const App = (a: OpenAPI3.AppObject): OpenAPI3.AppObject => a;
+export const App = (a: OpenAPI3.AppObject): OpenAPI3.AppObject => a
 
 export const Controller = (ctrl: { prefix: string; route: typeof Route }) => (
   app: Express
 ): OpenAPI3.PathItem[] => {
-  const paths = ctrl.route(express.Router());
-  app.use(ctrl.prefix, paths.router);
-  paths.paths.forEach(p => {
-    Object.keys(p).forEach(k => {
+  const paths = ctrl.route(express.Router())
+  app.use(ctrl.prefix, paths.router)
+  paths.paths.forEach((p) => {
+    Object.keys(p).forEach((k) => {
       p[`${ctrl.prefix}${k}`] = p[k]
       delete p[k]
     })
   })
-  return paths.paths;
-};
+  return paths.paths
+}
 
 export const Paths = (
   app: Express,
   ...ctrls: ReturnType<typeof Controller>[]
 ): OpenAPI3.PathItem =>
   ctrls.reduce((acc, c) => {
-    const paths = c(app);
-    paths.forEach((p) => (acc = { ...acc, ...p }));
-    return acc;
-  }, {});
+    const paths = c(app)
+    paths.forEach((p) => (acc = { ...acc, ...p }))
+    return acc
+  }, {})
 
 type AsyncRequestHandler = (
   req: Request,
   res: Response,
   next: NextFunction
-) => Promise<void>;
+) => Promise<void>
 
 export const AsyncWrapper = (cb: AsyncRequestHandler) => async (
   req: Request,
@@ -55,29 +54,29 @@ export const AsyncWrapper = (cb: AsyncRequestHandler) => async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    await cb(req, res, next);
+    await cb(req, res, next)
   } catch (e) {
-    next(e);
+    next(e)
   }
-};
+}
 
 export const ScopeWrapper = (
   cb: RequestHandler,
   scopes: OpenAPI3.ScopeHandler[]
 ) => (req: Request, res: Response, next: NextFunction): void => {
   if (scopes.some((v) => v(req, res, next))) {
-    next();
-    return;
+    next()
+    return
   } else {
-    cb(req, res, next);
+    cb(req, res, next)
   }
-};
+}
 
 export interface Security<S = string> {
-  name: string;
-  handler: RequestHandler;
-  scopes: OpenAPI3.NamedHandler<S>;
-  responses: OpenAPI3.MediaSchemaItem;
+  name: string
+  handler: RequestHandler
+  scopes: OpenAPI3.NamedHandler<S>
+  responses: OpenAPI3.MediaSchemaItem
 }
 
 export const Scope = <T = string>(
@@ -93,20 +92,20 @@ export const Scope = <T = string>(
     ),
   ],
   responses: security.responses,
-});
+})
 
 export const AuthPathOp = (scope: OpenAPI3.ScopeObject) => (
   pop: OpenAPI3.PathObject
 ): OpenAPI3.PathObject => {
-  const [m] = Object.keys(pop);
-  const ret: OpenAPI3.PathOperation = pop[m];
+  const [m] = Object.keys(pop)
+  const ret: OpenAPI3.PathOperation = pop[m]
   ret.security = {
     [scope.auth]: scope.scopes,
-  };
-  ret.scope = [scope];
-  ret.responses = { ...ret.responses, ...scope.responses };
-  return { [m]: ret };
-};
+  }
+  ret.scope = [scope]
+  ret.responses = { ...ret.responses, ...scope.responses }
+  return { [m]: ret }
+}
 
 export const Route = (
   rtr: Router,
@@ -121,142 +120,164 @@ export const Route = (
           path,
           method,
         })
-      );
-    });
-    return urtr;
+      )
+    })
+    return urtr
   }, rtr),
-});
+})
 
 const mapRouter = (
   urtr: Router,
   p: { pathOp: OpenAPI3.PathOperation; path: string; method: string }
 ) => {
-  const middle = [];
-  let wrapper = (cb: RequestHandler) => cb;
+  const middle = []
+  let wrapper = (cb: RequestHandler) => cb
   if (p.pathOp.wrapper) {
-    wrapper = p.pathOp.wrapper;
+    wrapper = p.pathOp.wrapper
   }
 
   // security handler
   if (p.pathOp.scope) {
     p.pathOp.scope.forEach((s: OpenAPI3.ScopeObject) => {
-      middle.push(...s.middleware.map(wrapper));
-    });
+      middle.push(...s.middleware.map(wrapper))
+    })
+  }
+
+  // validate 'application/json' POST body
+  if (
+    p.pathOp.requestBody &&
+    p.pathOp.requestBody.content['application/json']
+  ) {
+    const handler = validateHandler(
+      ajv.compile(p.pathOp.requestBody.content['application/json'].schema),
+      'body'
+    )
+    middle.push(wrapper(handler))
   }
 
   if (p.pathOp.parameters) {
-    const { handler } = validate(p.pathOp.parameters)
-    middle.push(wrapper(handler));
+    const { handlers } = validate(p.pathOp.parameters)
+    middle.push(...handlers.map(wrapper))
   }
 
-  middle.push(p.pathOp.middleware.map(wrapper));
-  urtr[p.method](p.path, middle);
-};
+  middle.push(p.pathOp.middleware.map(wrapper))
+  urtr[p.method](p.path, middle)
+  // cleanup
+  delete p.pathOp.middleware
+}
 
 export const Path = (
   path: string,
   ...po: OpenAPI3.PathObject[]
 ): OpenAPI3.PathItem => ({
   [path]: po.reduce((acc, p) => ({ ...acc, ...p }), {}),
-});
+})
 
 export const Method = (m: string) => (
   pop: OpenAPI3.PathOperation
 ): OpenAPI3.PathObject => ({
   [m]: pop,
-});
+})
 
 export const AsyncMethod = (
   m: string,
   wrapper: (cb: AsyncRequestHandler) => AsyncRequestHandler
 ) => (pop: OpenAPI3.PathOperation): OpenAPI3.PathObject => ({
   [m]: { wrapper, ...pop },
-});
+})
 
-export const Get = Method("get");
-export const Post = Method("post");
-export const Put = Method("put");
+export const Get = Method('get')
+export const Post = Method('post')
+export const Put = Method('put')
 
-export const AsyncGet = AsyncMethod("get", AsyncWrapper);
-export const AsyncPost = AsyncMethod("post", AsyncWrapper);
-export const AsyncPut = AsyncMethod("put", AsyncWrapper);
-export const AsyncDelete = AsyncMethod("delete", AsyncWrapper);
+export const AsyncGet = AsyncMethod('get', AsyncWrapper)
+export const AsyncPost = AsyncMethod('post', AsyncWrapper)
+export const AsyncPut = AsyncMethod('put', AsyncWrapper)
+export const AsyncDelete = AsyncMethod('delete', AsyncWrapper)
 
 export const Param = (pin: OpenAPI3.ParamIn) => (
-  param: Omit<OpenAPI3.Parameter, "in">
+  param: Omit<OpenAPI3.Parameter, 'in'>
 ): OpenAPI3.Parameter => ({
   in: pin,
   ...param,
-});
+})
 
-export const Query = Param("query");
-export const Body = Param("body");
+export const QueryParam = Param('query')
+export const PathParam = Param('path')
 
 export const Integer = (
   sch: Partial<OpenAPI3.ParamSchema>
 ): OpenAPI3.ParamSchema => ({
-  type: "integer" as OpenAPI3.ParamType,
+  type: 'integer' as OpenAPI3.ParamType,
   ...sch,
-});
+})
 
 export const validateParams = (
   p: Partial<OpenAPI3.Parameter>[]
 ): SchemaObject =>
   p.reduce<SchemaObject>(
     (acc, s) => {
-      acc.properties[s.name] = { ...s.schema };
+      acc.properties[s.name] = { ...s.schema }
       // base-level requirements
       if (s.required === true) {
         acc.required.push(s.name)
       }
-      return acc;
+      return acc
     },
-    { type: "object", properties: {}, required: [] } as OpenAPI3.ParamSchema
-  );
+    { type: 'object', properties: {}, required: [] } as OpenAPI3.ParamSchema
+  )
+
+const inMap = { path: 'params', query: 'query' }
 
 export const groupByParamIn = (params: OpenAPI3.Parameter[]) =>
   params.reduce((group, p) => {
-    if (!group[p.in]) {
-      group[p.in] = [];
+    const m = inMap[p.in]
+    if (!group[m]) {
+      group[m] = []
     }
-    group[p.in].push(p);
-    return group;
-  }, {} as { [key in OpenAPI3.ParamIn]: OpenAPI3.Parameter[] });
+    group[m].push(p)
+    return group
+  }, {} as { [key in OpenAPI3.ParamIn]: OpenAPI3.Parameter[] })
 
-type ValidateByParam = { [key in OpenAPI3.ParamIn]: ValidateFunction<unknown> };
+type ValidateByParam = { [key in OpenAPI3.ParamIn]: ValidateFunction<unknown> }
 
 export const validateBuilder = (v: Ajv) => (
   s: OpenAPI3.Parameter[]
 ): {
-  handler: ((req: Request, res: Response, next: NextFunction) => void),
-  schema: { [p: string]: SchemaObject },
+  handlers: ((req: Request, res: Response, next: NextFunction) => void)[]
+  schema: { [p: string]: SchemaObject }
 } => {
-  const pIns = groupByParamIn(s);
+  const pIns = groupByParamIn(s)
   const ret: { [p: string]: SchemaObject } = {}
-  const pKeys = Object.keys(pIns);
+  const pKeys = Object.keys(pIns)
   const validators: ValidateByParam = pKeys.reduce(
     (acc, k: OpenAPI3.ParamIn) => {
       const schema = validateParams(pIns[k])
-      acc[k] = v.compile(schema);
+      acc[k] = v.compile(schema)
       ret[k] = schema
-      return acc;
+      return acc
     },
     {} as ValidateByParam
-  );
+  )
   return {
-    handler: (req: Request, _res: Response, next: NextFunction) => {
-    pKeys.forEach((k) => {
-      if (!validators[k](req[k])) {
-        throw new ValidationError("AejoValidationError", validators[k].errors);
-      }
-    });
-    next();
-  },
-  schema: ret,
+    handlers: pKeys.map((k) =>
+      validateHandler(validators[k], k as OpenAPI3.ParamIn)
+    ),
+    schema: ret,
   }
-};
+}
 
-export const validate = validateBuilder(ajv);
+const validateHandler = (
+  valid: ValidateFunction,
+  whereIn: OpenAPI3.ParamIn
+) => (req: Request, _res: Response, next: NextFunction) => {
+  if (!valid(req[whereIn])) {
+    throw new ValidationError('AejoValidationError', valid.errors)
+  }
+  next()
+}
+
+export const validate = validateBuilder(ajv)
 
 /*
 export const docPath = (doc: JsonObject) => (
@@ -271,4 +292,4 @@ export default {
   validateParams,
   validateBuilder,
   // docPath,
-};
+}
