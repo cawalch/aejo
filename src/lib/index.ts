65b7203062f48a3ad2722726300a4a32f4d80a27
Caleb Walch <cawalch@gmail.com>
2022-01-15 14:21:40 -0600

    docs: code comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -14,10 +14,47 @@ import { ValidationError } from './errors'
 export const ajv = new Ajv()
 addFormats(ajv)
 
+/**
+ * AppRoute
+ *
+ * Binds Express Router with defined application routes
+ *
+ * ```typescript
+ * import { Router } from 'express'
+ *
+ * import { Route, Path, AppRoute } from 'aejo'
+ * import listRoute from './routes/list'
+ *
+ * export default (router: Router): AppRoute =>
+ *      Route(router,
+ *        Path(
+ *          '/',
+ *          listRoute
+ *        )
+ *      )
+ * ```
+ */
 export type AppRoute = { paths: OpenAPI3.PathItem[]; router: Router }
 
 export const App = (a: OpenAPI3.AppObject): OpenAPI3.AppObject => a
 
+/**
+ * Controller
+ *
+ * Creates a new controller with a path prefix
+ *
+ * ```typescript
+ * import { Controller } from 'aejo'
+ *
+ * import logsController from './logs'
+ *
+ * // map controller and path prefix
+ * Controller({
+ *   prefix: '/api/logs',
+ *   route: logsController
+ * })
+ * ```
+ */
 export const Controller =
   (ctrl: { prefix: string; route: typeof Route }) =>
   (app: Express): OpenAPI3.PathItem[] => {
@@ -32,6 +69,23 @@ export const Controller =
     return paths.paths
   }
 
+/**
+ * Paths
+ *
+ * Define path endpoints with Express
+ * app context
+ *
+ * ```typescript
+ * Paths(
+ *  // express app
+ *  app,
+ *  Controller({
+ *    prefix: '/api/logs',
+ *    route: logsController
+ *  })
+ * )
+ * ```
+ */
 export const Paths = (
   app: Express,
   ...ctrls: ReturnType<typeof Controller>[]
@@ -48,6 +102,11 @@ type AsyncRequestHandler = (
   next: NextFunction
 ) => Promise<void>
 
+/**
+ * AsyncWrapper
+ *
+ * Wraps Express RequestHandler's inside an async callback
+ */
 export const AsyncWrapper =
   (cb: AsyncRequestHandler) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -58,6 +117,12 @@ export const AsyncWrapper =
     }
   }
 
+/**
+ * ScopeWrapper
+ *
+ * Wraps Express RequestHandler's inside scoped middleware.
+ * Used to create middleware that maps to Security scopes.
+ */
 export const ScopeWrapper =
   (cb: RequestHandler, scopes: OpenAPI3.ScopeHandler[]) =>
   (req: Request, res: Response, next: NextFunction): void => {
@@ -77,6 +142,48 @@ export interface Security<S = string> {
   responses: OpenAPI3.MediaSchemaItem
 }
 
+/**
+ * Scope
+ *
+ * Create a user security scope
+ *
+ * ```typescript
+ * import { Request, Response } from 'express'
+ * import { Security, ScopeHandler } from 'aejo'
+ *
+ * // user session interface
+ * interface UserAuth extends Request {
+ *    user?: {
+ *      level: number
+ *    }
+ * }
+ *
+ * // authorization middleware based on user level
+ * const UserLevel = (minLevel: number): ScopeHandler => (
+ *    req: UserAuth
+ * ): boolean => req.user.level > minLevel
+ *
+ * const auth: Security = {
+ *  name: 'auth',
+ *  handler: (_req: Request, res: Response) => {
+ *    // unauthorized handler
+ *    res.status(400).send('Not Auth')
+ *  },
+ *  scopes: {
+ *    admin: UserLevel(100),
+ *    moderator: UserLevel(50)
+ *  },
+ *  responses: {
+ *  '400': {
+ *    description: 'Not Auth'
+ *   }
+ *  }
+ * }
+ *
+ * // build admin authorization middleware
+ * const AdminAuth = AuthPathOp(Scope(auth, 'admin'))
+ * ```
+ */
 export const Scope = <T = string>(
   security: Security<T>,
   ...scopes: (keyof OpenAPI3.NamedHandler<T>)[]
@@ -93,6 +200,25 @@ export const Scope = <T = string>(
   responses: security.responses,
 })
 
+/**
+ * AuthPathOp
+ *
+ * Authorization middleware builder.
+ *
+ * ```typescript
+ * // create admin authorization middleware guard
+ * const AdminAuth = AuthPathOp(Scope(auth, 'admin'))
+ *
+ * // secure route with admin authorization
+ * AdminAuth(
+ *  Get({
+ *    middleware: [
+ *      // express HTTP handler
+ *    ]
+ *  })
+ * )
+ * ```
+ */
 export const AuthPathOp =
   (scope: OpenAPI3.ScopeObject) =>
   (pop: OpenAPI3.PathObject): OpenAPI3.PathObject => {
@@ -105,6 +231,26 @@ export const AuthPathOp =
     return { [m]: ret }
   }
 
+/**
+ * Route
+ *
+ * Map an Express route to one or more Paths
+ *
+ * ```typescript
+ * // returns router and PathItems for swagger docs
+ * const { router, pathItems } = Route(
+ *  express.Router(),
+ *  Path(
+ *    '/users',
+ *    Get({
+ *       middleware: [
+ *        // express HTTP handler
+ *       ]
+ *    })
+ *    )
+ *  )
+ * ```
+ */
 export const Route = (
   rtr: Router,
   ...pitems: OpenAPI3.PathItem[]
@@ -162,6 +308,22 @@ const mapRouter = (
   urtr[p.method](p.path, middle)
 }
 
+/**
+ * Path
+ *
+ * Create a single Path operation
+ *
+ * The path may have multiple Path items
+ *
+ * ```typescript
+ * // Defines GET and POST routes
+ * Path(
+ *   '/users',
+ *   Get(listUsers),
+ *   Post(createUser)
+ *  )
+ * ```
+ */
 export const Path = (
   path: string,
   ...po: OpenAPI3.PathObject[]
@@ -169,12 +331,27 @@ export const Path = (
   [path]: po.reduce((acc, p) => ({ ...acc, ...p }), {}),
 })
 
+/**
+ * Method
+ *
+ * Define a PathOperation based on an HTTP method
+ *
+ * ```typescript
+ * // Create `GET` method
+ * const Get = Method('get')
+ * ```
+ */
 export const Method =
   (m: string) =>
   (pop: OpenAPI3.PathOperation): OpenAPI3.PathObject => ({
     [m]: pop,
   })
 
+/**
+ * AsyncMethod
+ *
+ * Wrapper for calling AsyncMethods
+ */
 export const AsyncMethod =
   (m: string, wrapper: (cb: AsyncRequestHandler) => AsyncRequestHandler) =>
   (pop: OpenAPI3.PathOperation): OpenAPI3.PathObject => ({
@@ -190,6 +367,13 @@ export const AsyncPost = AsyncMethod('post', AsyncWrapper)
 export const AsyncPut = AsyncMethod('put', AsyncWrapper)
 export const AsyncDelete = AsyncMethod('delete', AsyncWrapper)
 
+/**
+ * Param
+ *
+ * OpenAPI3 param type builder
+ *
+ * See {@link QueryParam} and {@link PathParam}
+ */
 export const Param =
   (pin: OpenAPI3.ParamIn) =>
   (param: Omit<OpenAPI3.Parameter, 'in'>): OpenAPI3.Parameter => ({
@@ -197,7 +381,51 @@ export const Param =
     ...param,
   })
 
+
+/**
+ * QueryParam
+ *
+ * OpenAPI3 QueryParam Schema builder
+ *
+ * ```typescript
+ * Get({
+ *   parameters: [
+ *     // accept `/path?limit=<number>`
+ *     QueryParam({
+ *        name: 'limit',
+ *        description: 'max number',
+ *        schema: {
+ *          type: 'integer',
+ *          minimum: 1,
+ *        },
+ *     })
+ *   ]
+ * })
+ * ```
+ */
 export const QueryParam = Param('query')
+
+/**
+ * PathParam
+ *
+ * OpenAPI3 PathParam Schema builder
+ *
+ * ```typescript
+ * Put({
+ *  parameters: [
+ *    // accept `/path/:id` where `:id` is in uuidv4 format
+ *    PathParam({
+ *      name: 'id',
+ *      description: 'user id',
+ *      schema: {
+ *        type: 'string',
+ *        format: 'uuidv4'
+ *      }
+ *    })
+ *  ]
+ * })
+ * ```
+ */
 export const PathParam = Param('path')
 
 export const Integer = (
